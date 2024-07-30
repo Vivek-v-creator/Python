77160e334f1ab8822ce6c10f079525fd164ea3de
Vivekanand Fakirapur <anandfakirapur99@gmail.com>
2024-07-30 14:55:54 +0530

    Add files via upload

---
+++ ingrr.py
@@ -0,0 +1,14 @@
+e=str(input("enter the recipe name :"))
+n=int(input("Enter no of members :"))
+ingr1=(input("enter 1st ingr :"))
+quantity1=float(input("enter quantity :"))
+ingr2=(input("enter 2nd ingr :"))
+quantity2=float(input("enter quantity :"))
+ingr3=(input("enter 3rd ingr :"))
+quantity3=float(input("enter quantity :"))
+recipe1=n*quantity1
+recipe2=n*quantity2
+recipe3=n*quantity3
+print(f"{recipe1}")
+print(f"{recipe2}")
+print(f"{recipe3}")
